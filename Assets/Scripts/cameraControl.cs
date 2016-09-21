58eda908de7b16882b4f76a12942beedb37d44c7
Morgan Erlich <nagrom525@gmail.com>
2016-09-20 20:59:52 -0400

    yay

--- Assets/Scripts/cameraControl.cs
+++ Assets/Scripts/cameraControl.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class cameraControl : MonoBehaviour {
-
+    private 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void MoveCamera(Direction d) {
+
+    }
 }
