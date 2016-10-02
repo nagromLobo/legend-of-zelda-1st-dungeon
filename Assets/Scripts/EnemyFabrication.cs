efee03b6b42644d9d2a494331c4fd6bdba31579f
Morgan Erlich <nagrom525@gmail.com>
2016-10-02 19:03:12 -0400

    Enemies placed

--- Assets/Scripts/EnemyFabrication.cs
+++ Assets/Scripts/EnemyFabrication.cs
@@ -56,36 +56,37 @@ public class EnemyFabrication : MonoBehaviour {
             spawnGrid[i] = new List<Vector3>();
         }
         // for testing wall masters
-        spawnGrid[0] = new List<Vector3> { new Vector3(39.5f, 6.0f, 0.0f)
-        };
-        spawnGrid[1] = new List<Vector3> { new Vector3(19.0f, 7.0f, 0.0f),
+        //spawnGrid[0] = new List<Vector3> { new Vector3(39.5f, 6.0f, 0.0f)
+        //};
+        if (!customLevel) {
+            spawnGrid[1] = new List<Vector3> { new Vector3(19.0f, 7.0f, 0.0f),
                                             new Vector3(18.0f, 5.0f, 0.0f),
                                             new Vector3(26.0f, 2.0f, 0.0f)
-        };
-        spawnGrid[2] = new List<Vector3> { new Vector3(52.0f, 7.0f, 0.0f),
+            };
+            spawnGrid[2] = new List<Vector3> { new Vector3(52.0f, 7.0f, 0.0f),
                                             new Vector3(54.0f, 3.0f, 0.0f),
                                             new Vector3(57.0f, 5.0f, 0.0f),
                                             new Vector3(61.0f, 5.0f, 0.0f),
                                             new Vector3(61.0f, 2.0f, 0.0f)
-        };
-        spawnGrid[3] = new List<Vector3> { new Vector3(41.0f, 19.0f, 0.0f),
+            };
+            spawnGrid[3] = new List<Vector3> { new Vector3(41.0f, 19.0f, 0.0f),
                                             new Vector3(43.0f, 17.0f, 0.0f),
                                             new Vector3(36.0f, 17.0f, 0.0f)
-        };
-        spawnGrid[4] = new List<Vector3> { new Vector3(34.0f, 29.0f, 0.0f),
+            };
+            spawnGrid[4] = new List<Vector3> { new Vector3(34.0f, 29.0f, 0.0f),
                                             new Vector3(35.0f, 28.0f, 0.0f),
                                             new Vector3(35.0f, 27.0f, 0.0f),
                                             new Vector3(37.0f, 29.0f, 0.0f),
                                             new Vector3(37.0f, 27.0f, 0.0f)
-        };
-        spawnGrid[5] = new List<Vector3> { new Vector3(26.0f, 30.0f, 0.0f),
+            };
+            spawnGrid[5] = new List<Vector3> { new Vector3(26.0f, 30.0f, 0.0f),
                                             new Vector3(18.5f, 29.0f, 0.0f),
                                             new Vector3(18.5f, 27.0f, 0.0f),
                                             new Vector3(20.5f, 28.0f, 0.0f),
                                             new Vector3(21.0f, 26.5f, 0.0f),
                                             new Vector3(19.0f, 24.5f, 0.0f)
-        };
-        spawnGrid[6] = new List<Vector3> { new Vector3(53.0f, 30.0f, 0.0f),
+            };
+            spawnGrid[6] = new List<Vector3> { new Vector3(53.0f, 30.0f, 0.0f),
                                             new Vector3(53.0f, 24.0f, 0.0f),
                                             new Vector3(56.0f, 28.0f, 0.0f),
                                             new Vector3(56.0f, 26.0f, 0.0f),
@@ -93,39 +94,100 @@ public class EnemyFabrication : MonoBehaviour {
                                             new Vector3(58.0f, 28.0f, 0.0f),
                                             new Vector3(60.0f, 27.0f, 0.0f),
                                             new Vector3(60.0f, 23.0f, 0.0f)
-        };
-        spawnGrid[7] = new List<Vector3> { new Vector3(24.0f, 36.0f, 0.0f),
+            };
+            spawnGrid[7] = new List<Vector3> { new Vector3(24.0f, 36.0f, 0.0f),
                                             new Vector3(25.0f, 40.0f, 0.0f),
                                             new Vector3(23.0f, 40.0f, 0.0f)
-        };
-        spawnGrid[8] = new List<Vector3> { new Vector3(35.0f, 41.0f, 0.0f),
+            };
+            spawnGrid[8] = new List<Vector3> { new Vector3(35.0f, 41.0f, 0.0f),
                                             new Vector3(45.0f, 41.0f, 0.0f),
                                             new Vector3(45.0f, 35.0f, 0.0f),
                                             new Vector3(42.0f, 36.0f, 0.0f)
-        };
-        spawnGrid[9] = new List<Vector3> { new Vector3(35.0f, 51.0f, 0.0f),
+            };
+            spawnGrid[9] = new List<Vector3> { new Vector3(35.0f, 51.0f, 0.0f),
                                             new Vector3(42.0f, 50.0f, 0.0f),
                                             new Vector3(44.0f, 49.0f, 0.0f)
-        };
-        spawnGrid[10] = new List<Vector3> { new Vector3(41.0f, 63.0f, 0.0f),
+            };
+            spawnGrid[10] = new List<Vector3> { new Vector3(41.0f, 63.0f, 0.0f),
                                             new Vector3(40.0f, 61.0f, 0.0f),
                                             new Vector3(38.0f, 62.0f, 0.0f)
-        };
-        spawnGrid[11] = new List<Vector3> { new Vector3(29.0f, 63.0f, 0.0f),
+            };
+            spawnGrid[11] = new List<Vector3> { new Vector3(29.0f, 63.0f, 0.0f),
                                             new Vector3(18.0f, 63.0f, 0.0f),
                                             new Vector3(29.0f, 57.0f, 0.0f),
                                             new Vector3(18.0f, 57.0f, 0.0f)
-        };
-        spawnGrid[12] = new List<Vector3> { new Vector3(51.0f, 37.0f, 0.0f),
+            };
+            spawnGrid[12] = new List<Vector3> { new Vector3(51.0f, 37.0f, 0.0f),
                                             new Vector3(52.0f, 40.0f, 0.0f),
                                             new Vector3(53.0f, 40.0f, 0.0f)
-        };
-        spawnGrid[13] = new List<Vector3> { new Vector3(71.5f, 38.0f, 0.0f) };
-        spawnGrid[14] = new List<Vector3> { new Vector3(75.0f, 49.0f, 0.0f)
-        };
-        spawnGrid[15] = new List<Vector3> { new Vector3(4.0f, 38.0f, 0.0f),
+            };
+            spawnGrid[13] = new List<Vector3> { new Vector3(71.5f, 38.0f, 0.0f) };
+            spawnGrid[14] = new List<Vector3> { new Vector3(75.0f, 49.0f, 0.0f)
+            };
+            spawnGrid[15] = new List<Vector3> { new Vector3(4.0f, 38.0f, 0.0f),
                                             new Vector3(10.0f, 38.0f, 0.0f)
-        };
+            };
+        } else {
+            spawnGrid[1] = new List<Vector3> { new Vector3(38.0f, 15.0f, 0.0f),
+                                            new Vector3(42.0f, 15.0f, 0.0f),
+                                            new Vector3(42.0f, 17.0f, 0.0f),
+                                            new Vector3(38.0f, 17.0f, 0.0f)
+            };
+            spawnGrid[2] = new List<Vector3> { new Vector3(39.0f, 24.0f, 0.0f),
+                                            new Vector3(44.0f, 25.0f, 0.0f),
+                                            new Vector3(44.0f, 30.0f, 0.0f),
+                                            new Vector3(38.0f, 30.0f, 0.0f),
+                                            new Vector3(36.0f, 29.0f, 0.0f)
+            };
+            spawnGrid[3] = new List<Vector3> { new Vector3(27.0f, 27.0f, 0.0f),
+                                            new Vector3(28.0f, 30.0f, 0.0f),
+                                            new Vector3(20.0f, 30.0f, 0.0f),
+                                            new Vector3(20.0f, 27.0f, 0.0f),
+                                            new Vector3(20.0f, 24.0f, 0.0f)
+            };
+            spawnGrid[4] = new List<Vector3> { new Vector3(13.0f, 30.0f, 0.0f),
+                                            new Vector3(8.0f, 25.0f, 0.0f),
+                                            new Vector3(4.0f, 28.0f, 0.0f)
+            };
+            spawnGrid[5] = new List<Vector3> { new Vector3(51.0f, 25.0f, 0.0f),
+                                            new Vector3(52.0f, 25.0f, 0.0f),
+                                            new Vector3(54.0f, 29.0f, 0.0f),
+                                            new Vector3(57.0f, 29.0f, 0.0f),
+                                            new Vector3(60.0f, 25.0f, 0.0f)
+            };
+            spawnGrid[6] = new List<Vector3> { new Vector3(69.0f, 28.0f, 0.0f),
+                                            new Vector3(74.0f, 28.0f, 0.0f),
+                                            new Vector3(71.0f, 24.0f, 0.0f)
+            };
+            spawnGrid[7] = new List<Vector3> { new Vector3(4.0f, 35.0f, 0.0f),
+                                            new Vector3(11.0f, 35.0f, 0.0f)
+            };
+            spawnGrid[8] = new List<Vector3> { new Vector3(4.0f, 46.0f, 0.0f)
+            };
+            spawnGrid[9] = new List<Vector3> { new Vector3(19.0f, 50.0f, 0.0f),
+                                            new Vector3(23.0f, 51.0f, 0.0f),
+                                            new Vector3(22.0f, 47.0f, 0.0f),
+                                            new Vector3(28.0f, 46.0f, 0.0f)
+            };
+            spawnGrid[10] = new List<Vector3> { new Vector3(41.0f, 63.0f, 0.0f),
+                                            new Vector3(40.0f, 61.0f, 0.0f),
+                                            new Vector3(38.0f, 62.0f, 0.0f)
+            };
+            spawnGrid[11] = new List<Vector3> { new Vector3(76.0f, 51.0f, 0.0f),
+                                            new Vector3(68.0f, 47.0f, 0.0f)
+            };
+            spawnGrid[12] = new List<Vector3> { new Vector3(51.0f, 51.0f, 0.0f),
+                                            new Vector3(51.0f, 46.0f, 0.0f),
+                                            new Vector3(54.0f, 48.0f, 0.0f),
+                                            new Vector3(60.0f, 51.0f, 0.0f),
+                                            new Vector3(59.0f, 46.0f, 0.0f)
+            };
+            spawnGrid[13] = new List<Vector3> { new Vector3(39.5f, 49.0f, 0.0f) };
+            spawnGrid[14] = new List<Vector3> { new Vector3(42.0f, 37.0f, 0.0f),
+                                                new Vector3(36.0f, 38.0f, 0.0f)
+            };
+        }
+        
         CameraControl.S.cameraMoveCompleteDelegate += CameraMoveComplete;
         CameraControl.S.cameraMovedDelegate += OnStartCameraMovement;
 
